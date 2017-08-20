906f84de39d362949eda52a1fbf4a1bb74ac6e42
Emmet O'Grady <emmet789@gmail.com>
2017-08-20 12:29:44 +0100

    Use CasperJS instead of Behat in web functional test

--- tests/web/run_integration_test.sh
+++ tests/web/run_integration_test.sh
@@ -30,7 +30,8 @@ docker run --rm -it --net web_default -v $DIR/db-preload-data.js:/db-preload-dat
 
 # Run tests
 echo -e "\n>>>> RUNNING TESTS"
-docker run --rm -it --net web_default -v $DIR/behat:/root/project tvial/behat behat
+#docker run --rm -it --net web_default -v $DIR/behat:/root/project tvial/behat behat
+docker run --rm -it --net web_default -v $DIR/casperjs:/data zopanix/casperjs test --log-level=debug --verbose=true homepage.js
 
 if [ "$?" != "0" ]; then
     echo "Integration tests FAILED :("
